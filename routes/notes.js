7a4faa001bd2c718b48cd0a712b32afe3980aeae
Lina Quintana <lquintana479@gmail.com>
2023-09-01 13:02:28 -0400

    added files to vs code needed for challenge and updated the code for index.js, server.js, and notes.js with correct code needed for this challenge

---
+++ routes/notes.js
@@ -0,0 +1,66 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
+
+// api end point
+// GET Route for retrieving all the tips
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// GET Route for a specific tip
+notes.get('/:notes_id', (req, res) => {
+  const notesId = req.params.notes_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((notes) => notes.notes_id === notesId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json('No notes with that ID');
+    });
+});
+
+// DELETE Route for a specific tip
+notes.delete('/:notes_id', (req, res) => {
+  const notesId = req.params.notes_id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((notes) => notes.notes_id !== notesId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/db.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${notesId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new UX/UI tip
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { username, topic, notes } = req.body;
+
+  if (req.body) {
+    const newTip = {
+      username,
+      notes,
+      topic,
+      tip_id: uuidv4(),
+    };
+
+    readAndAppend(newNotes, './db/db.json');
+    res.json(`Notes added successfully`);
+  } else {
+    res.error('Error in adding notes');
+  }
+});
+
+module.exports = notes;
